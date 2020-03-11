e918213e69793898e01c5b3612b4ef3765a9a72d
Chuka <b.chuka@gmail.com>
2020-03-11 17:11:25 +0800

    client area feature hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,14 @@ import "lazysizes";
 import MobileMenu from "./modules/MobileMenu.js";
 import RevealOnScroll from "./modules/RevealOnScroll.js";
 import StickyHeader from "./modules/StickyHeader.js";
+import ClientArea from "./modules/ClientArea.js";
 
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
 
 let mobileMenu = new MobileMenu();
+new ClientArea();
 
 let modal;
 
